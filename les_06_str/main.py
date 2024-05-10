c798218aa7b825d903b23b80b14b70cb9902771b
Max Zarev <maksimelizarev@gmail.com>
2024-05-10 17:04:19 +0700

    add 6 lesson

---
+++ les_06_str/main.py
@@ -0,0 +1,19 @@
+str
+
+# text = 'one two three'
+# one_var, two_var, three_var = text.split(" ")
+#
+# print(one_var)
+# print(two_var)
+# print(three_var)
+
+text = 'one two three'
+print(text.endswith('two'))
+
+
+
+
+
+
+
+
