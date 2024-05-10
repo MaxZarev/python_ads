cd13436f4ad8b68dd131496987ef10a10d356f94
Max Zarev <maksimelizarev@gmail.com>
2024-05-10 17:50:54 +0700

    add 7 lesson

---
+++ les_07_str_methods/main.py
@@ -0,0 +1,23 @@
+
+# password = "12345678"
+# password_len = len(password)
+# print(password_len)
+
+# text = "Hello, World!"
+# text2 = "my name is Max"
+# print(1, 2, sep="-", end="\n-----")
+# print(text, text2, text, text2)
+# color1 = "Green"
+# color2 = "green"
+# print(color1.lower() == color2.lower())
+# print(color1.lower() != color2.lower())
+# print(f"a > a = ", color1 > color2)
+# print(f"a < b = ", color1 < color2)
+
+# text = "Hello, World!"
+# print("llo" in "Hello, World!") # в
+
+
+
+
+
